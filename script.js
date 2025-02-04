ddf79e5a4a5f6d5385e4860764fe888d78d2c613
Felipe <126893383+feoliveira7@users.noreply.github.com>
2025-02-03 23:41:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const tampa = document.getElementById('superiorFrame')
+
+function login(){
+    tampa.style.marginLeft = '400px'
+    document.getElementById('CriarDisplay').style.display='flex'
+document.getElementById('logarDisplay').style.display='none'
+}
+
+function cadastro(){
+tampa.style.marginLeft = '0px'
+document.getElementById('CriarDisplay').style.display='none'
+document.getElementById('logarDisplay').style.display='flex'
+}
